7a5768ebcc9989497e3537668b20aed6fec75cab
webbridgsolutions <webbridgsolucions@gmail.com>
2026-07-09 12:39:28 -0600

    Update index.js

--- index.js
+++ index.js
@@ -300,7 +300,7 @@ app.post('/translate-bridge', async (req, res) => {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                model: "llama-3.3-70b-versatile",
+                model: "llama-3.1-8b-instant",
                 messages: [
                     { 
                       role: "system", 
